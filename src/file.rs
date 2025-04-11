3feb4d6208c914de56798cf462b90caca2c2b141
Alan Sartorio <alan42ga@hotmail.com>
2025-04-10 23:54:31 -0700

    refactor: move file management utils to module

---
+++ src/file.rs
@@ -0,0 +1,56 @@
+use std::{env::current_dir, fs::{self, File}, os, path::PathBuf, str::FromStr};
+
+use anyhow::{anyhow, Result};
+
+fn find_clockin_file() -> Option<PathBuf> {
+    let first_dir = current_dir().unwrap();
+    let mut maybe_dir = Some(first_dir.as_path());
+
+    while let Some(dir) = maybe_dir {
+        let mut file = dir.to_owned();
+        file.push(".clockin");
+        if file.exists() {
+            return Some(file);
+        }
+        maybe_dir = dir.parent();
+    }
+
+    None
+}
+
+fn get_var_path(name: &str) -> Option<PathBuf> {
+    std::env::var(name)
+        .map(|home| PathBuf::from_str(home.as_str()).unwrap())
+        .ok()
+}
+
+fn get_data_dir() -> PathBuf {
+    let mut data = get_var_path("XDG_DATA_HOME")
+        .or_else(|| {
+            get_var_path("HOME").map(|mut home| {
+                home.push(".local/share");
+                home
+            })
+        })
+        .unwrap();
+    data.push("clockin");
+    fs::create_dir_all(&data).unwrap();
+    data
+}
+
+pub fn create_clockin_file(name: &str) -> Result<PathBuf> {
+    let mut data = get_data_dir();
+    data.push(name);
+    let clockin_link = PathBuf::from_str(".clockin").unwrap();
+    File::options()
+        .write(true)
+        .create(true)
+        .truncate(false)
+        .open(&data)?;
+    os::unix::fs::symlink(&data, &clockin_link)?;
+    Ok(clockin_link)
+}
+
+pub fn require_clockin_file() -> Result<PathBuf> {
+    find_clockin_file().ok_or(anyhow!(".clockin file not found"))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
 use std::{
-    env::{Args, args, current_dir},
-    fs::{self, File},
-    io::{Write, stdin},
-    os,
-    path::PathBuf,
+    env::{Args, args},
+    io::stdin,
     process,
-    str::FromStr,
     sync::mpsc::{self, Receiver},
     thread,
 };
 
 use anyhow::{Context, Result, anyhow};
-use chrono::Local;
 use writer::Writer;
 
+mod file;
 mod parser;
 mod writer;
 
@@ -46,60 +42,6 @@ fn parse_args(args: Args) -> Result<Command> {
         command => Err(anyhow!("invalid command {command}")),
     }
 }
-
-fn find_clockin_file() -> Option<PathBuf> {
-    let first_dir = current_dir().unwrap();
-    let mut maybe_dir = Some(first_dir.as_path());
-
-    while let Some(dir) = maybe_dir {
-        let mut file = dir.to_owned();
-        file.push(".clockin");
-        if file.exists() {
-            return Some(file);
-        }
-        maybe_dir = dir.parent();
-    }
-
-    None
-}
-
-fn get_var_path(name: &str) -> Option<PathBuf> {
-    std::env::var(name)
-        .map(|home| PathBuf::from_str(home.as_str()).unwrap())
-        .ok()
-}
-
-fn get_data_dir() -> PathBuf {
-    let mut data = get_var_path("XDG_DATA_HOME")
-        .or_else(|| {
-            get_var_path("HOME").map(|mut home| {
-                home.push(".local/share");
-                home
-            })
-        })
-        .unwrap();
-    data.push("clockin");
-    fs::create_dir_all(&data).unwrap();
-    data
-}
-
-fn create_clockin_file(name: &str) -> Result<PathBuf> {
-    let mut data = get_data_dir();
-    data.push(name);
-    let clockin_link = PathBuf::from_str(".clockin").unwrap();
-    File::options()
-        .write(true)
-        .create(true)
-        .truncate(false)
-        .open(&data)?;
-    os::unix::fs::symlink(&data, &clockin_link)?;
-    Ok(clockin_link)
-}
-
-fn require_clockin_file() -> Result<PathBuf> {
-    find_clockin_file().ok_or(anyhow!(".clockin file not found"))
-}
-
 fn lines(cancel: Receiver<()>) -> Receiver<Option<String>> {
     let (sender, receiver) = mpsc::channel();
     let sender2 = sender.clone();
@@ -122,10 +64,10 @@ fn lines(cancel: Receiver<()>) -> Receiver<Option<String>> {
 fn run(command: Command, cancel: Receiver<()>) -> Result<()> {
     match &command {
         Command::Link { name } => {
-            create_clockin_file(name)?;
+            file::create_clockin_file(name)?;
         }
         Command::Edit => {
-            let path = require_clockin_file()?;
+            let path = file::require_clockin_file()?;
             let editor = std::env::var("EDITOR").unwrap_or("nano".to_owned());
             let mut process = process::Command::new(editor)
                 .arg(path)
@@ -134,7 +76,7 @@ fn run(command: Command, cancel: Receiver<()>) -> Result<()> {
             process.wait().context("error while editing file")?;
         }
         Command::ClockIn => {
-            let mut writer = Writer::new(require_clockin_file()?)?;
+            let mut writer = Writer::new(file::require_clockin_file()?)?;
             println!(
                 "{}",
                 concat!("==============\n", "= CLOCKED IN =\n", "==============")
@@ -148,7 +90,7 @@ fn run(command: Command, cancel: Receiver<()>) -> Result<()> {
             writer.end()?;
         }
         Command::Summary => {
-            let path = require_clockin_file()?;
+            let path = file::require_clockin_file()?;
             for session in parser::parse_file(path).unwrap() {
                 let duration = session.duration().to_std().unwrap().as_secs();
                 let hours = duration / (60 * 60);
