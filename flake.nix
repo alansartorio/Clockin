45a439daaf4d6076acf038e545958441fb989194
Alan Gabriel Sartorio <alan42ga@hotmail.com>
2026-02-27 02:51:33 -0300

    bump version to 0.6.0

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
                 installShellFiles,
               }:
               pkgs.rustPlatform.buildRustPackage {
-                version = "0.5.4";
+                version = "0.6.0";
                 pname = "clockin";
                 cargoLock.lockFile = ./Cargo.lock;
                 src = pkgs.lib.cleanSourceWith {
